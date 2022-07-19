7108eabfb1c7c05c813842c9d1c99b81bb25b428
lucaskruger10 <lucaskruger10@gmail.com>
2022-07-19 00:16:26 -0300

    added new energy experiments

--- CrossValidation.py
+++ CrossValidation.py
@@ -1,17 +1,19 @@
+from statistics import mean
 from Model import Model
 import numpy as np
 import random
 import timeit
 
+
 class CrossValidation():
     # ================ CONSTRUCTOR ========================
-    def __init__(self, X, Y, trainPercentage, S, funcArray = [], learningRate = 0.1, iters = 100, epsilon = 0.1, max_epochs = 1):
+    def __init__(self, X, Y, trainPercentage, S, funcArray=[], learningRate=0.1, iters=100, epsilon=0.1, max_epochs=1):
         self.X = X
         self.Y = Y
         self.percentage = trainPercentage
         self.indexes = range(X.shape[0])
         self.S = S
-        self.learningRate  = learningRate
+        self.learningRate = learningRate
         self.funcArray = funcArray
         self.iters = iters
         self.epsilon = epsilon
@@ -19,7 +21,8 @@ class CrossValidation():
 
     # ================ FUNCTIONS ========================
     def split(self):
-        index = np.random.choice(self.indexes, int(len(self.indexes)*self.percentage), replace = False)
+        index = np.random.choice(self.indexes, int(
+            len(self.indexes)*self.percentage), replace=False)
         not_index = np.setdiff1d(self.indexes, index)
         x_train = [self.X[i] for i in index]
         x_test = [self.X[i] for i in not_index]
@@ -37,38 +40,30 @@ class CrossValidation():
 
             x_train, x_test, y_train, y_test = self.split()
             model = Model(self.S, self.funcArray, self.learningRate)
-            #for j in range(self.epoch):
+            # for j in range(self.epoch):
             epoch = 0
-            n=0
 
             y_pred = model.predict(x_test)
             lastMeanError = self.meanError(y_pred, y_test)
             start = timeit.default_timer()
-            while(True): # this is the way to emulate a do while in python
-                epoch = epoch +1
-                model.train(x_train, y_train)
+
+            while(True):  # this is the way to emulate a do while in python
+                epoch = epoch + 1
+                x, y, learning = model.train(x_train, y_train)
                 y_pred = model.predict(x_test)
                 meanError = self.meanError(y_pred, y_test)
-                print(f"Epoch: {epoch} with error:{meanError} in Iter {i}")
-                if (meanError > lastMeanError):
-                    n +=1
-                else: 
-                    n = 0
-                lastMeanError = meanError
-                if (n == maxDec):
-                    x_train, y_train = self.shuffleBoth(x_train, y_train)
-                    n = 0
-                if (epoch % 300 == 0): 
-                    x_train, y_train = self.shuffleBoth(x_train, y_train)
+                print(
+                    f"Epoch: {epoch} with error:{meanError} with mean: {np.mean(meanError)} in Iter {i}")
 
-                if (meanError < self.epsilon or self.max_epochs < epoch): break #break on the error
+                if (np.mean(meanError) < self.epsilon or self.max_epochs < epoch):
+                    break  # break on the error
 
             meanErrors.append(meanError)
             epochs.append(epoch)
             times.append(start - timeit.default_timer())
-        return np.mean(np.array(meanErrors), axis=0), np.mean(np.array(epochs)),  np.mean(np.array(times))
+        return np.mean(np.array(meanErrors), axis=0), np.mean(np.array(epochs)),  np.mean(np.array(times), learning)
 
-    def shuffleBoth(self,x_train, y_train):
+    def shuffleBoth(self, x_train, y_train):
         temp = list(zip(x_train, y_train))
         random.shuffle(temp)
         res1, res2 = zip(*temp)
--- Model.py
+++ Model.py
@@ -2,86 +2,114 @@
 
 import numpy as np
 
+
 class Model():
-    def __init__(self, S = [], activationFuncArray = [], learningRate = 0.1, maxIter = 500):
-    # ========================= VARS =======================
-        self.S = S                                                      # array with number of neurons per inner layer
-        self.layers = len(S)                                            # number of layers
-        self.W = self.createRandomWeights()                             # array of weight matrices
+    def __init__(self, S=[], activationFuncArray=[], learningRate=0.001, maxIter=1000):
+        # ========================= VARS =======================
+        # array with number of neurons per inner layer
+        self.S = S
+        # number of layers
+        self.layers = len(S)
+        # array of weight matrices
+        self.W = self.createRandomWeights()
         self.learningRate = learningRate                                # learning rate
         self.maxIter = maxIter
-        if (activationFuncArray == []):                                 # default funcArray uses sigmoid for every layer
-            activationFuncArray = ["sigmoid"]*len(S)                    # set default activation array
-        self.activationFuncArray = activationFuncArray                  # activation function for each layers
+        # default funcArray uses sigmoid for every layer
+        if (activationFuncArray == []):
+            # set default activation array
+            activationFuncArray = ["sigmoid"]*len(S)
+        # activation function for each layers
+        self.activationFuncArray = activationFuncArray
         self.activationFuncDicc = {
-            "sigmoid" : self.sigmoid,
-            "tanh" : self.tanh,
-            "relu" : self.relu,
-            "step" : self.step
+            "sigmoid": self.sigmoid,
+            "tanh": self.tanh,
+            "relu": self.relu,
+            "step": self.step
         }
-        
+
     # ========================= FUNCTIONS   =========================
-    
-    def feedForward(self, Xh):                                          # Xh is what we're trying to predict
-        Y = []                                                          # Y is the output of each layer
+
+    # Xh is what we're trying to predict
+    def feedForward(self, Xh):
+        # Y is the output of each layer
+        Y = []
         dY = [0]
-        T = np.array(Xh)                                                # T is the input of the next layer
-        for k in range(1, self.layers):                                 # k is the current layer index
-            func = self.activationFuncDicc[self.activationFuncArray[k-1]] # get activation function
-            Y.append(self.addBias(T))                                   # add bias to the input
-            T, dT = func(np.dot(Y[k-1], self.W[k-1]))                   # calculate the output of the layer
-            dY.append(np.array(dT))                                               # save the derivative of the output
-        Y.append(np.array(T[0]))                                        # add the output of the last layer
-        return Y, dY                                                    # return the output of each layer
+        # T is the input of the next layer
+        T = np.array(Xh)
+        # k is the current layer index
+        for k in range(1, self.layers):
+            # get activation function
+            func = self.activationFuncDicc[self.activationFuncArray[k-1]]
+            # add bias to the input
+            Y.append(self.addBias(T))
+            # calculate the output of the layer
+            T, dT = func(np.dot(Y[k-1], self.W[k-1]))
+            # save the derivative of the output
+            dY.append(np.array(dT))
+        # add the output of the last layer
+        Y.append(np.array(T[0]))
+        # return the output of each layer
+        return Y, dY
 
-    def backPropagation(self, Y, dY, dZ):                                                                         # Y are the activation levels and Z are the desired output
-        dW = [None] * self.layers                                                                                   # delta W is the change in the weight matrix
-        E = np.array(dZ - Y[self.layers - 1])                                                                     # error is the difference between the desired output and the output of the last layer
-        D = [None] * self.layers                                                                                    # initialization of D as an empty array
-        D[self.layers-1] = np.multiply(E, dY[self.layers - 1])                                                      # dY is the derivative of the output of L-1 layer, and D[layers-1] is the product of E and dY
+    # Y are the activation levels and Z are the desired output
+    def backPropagation(self, Y, dY, dZ):
+        # delta W is the change in the weight matrix
+        dW = [None] * self.layers
+        # error is the difference between the desired output and the output of the last layer
+        E = np.array(dZ - Y[self.layers - 1])
+        # initialization of D as an empty array
+        D = [None] * self.layers
+        # dY is the derivative of the output of L-1 layer, and D[layers-1] is the product of E and dY
+        D[self.layers-1] = np.multiply(E, dY[self.layers - 1])
         for k in np.flip(range(1, self.layers)):
-            dW[k-1] = self.learningRate*np.dot(np.transpose(np.reshape(Y[k-1], (1,Y[k-1].shape[0]))), D[k])         # dW is the change in the weight matrix
-            E = np.dot(D[k], np.transpose(self.W[k-1]))                                                             # E is the error of the next layer = dW * dY
+            # dW is the change in the weight matrix
+            dW[k-1] = self.learningRate * \
+                np.dot(np.transpose(np.reshape(
+                    Y[k-1], (1, Y[k-1].shape[0]))), D[k])
+            # E is the error of the next layer = dW * dY
+            E = np.dot(D[k], np.transpose(self.W[k-1]))
             D[k-1] = np.multiply(self.subBias(E), dY[k-1])
-        return dW                                                                                                   # return the change in the weight matrix
-    
-
+        # return the change in the weight matrix
+        return dW
 
     def train(self, X, Z):
         iters = 0
         meanError = 1
         learning = []
-        while iters < self.maxIter and meanError > 0.1:
-            errors = []
-            for h in range(0,len(X)):
+        while iters < self.maxIter and np.mean(meanError) > 0.01:
+
+            for h in range(0, len(X)):
                 Y, dY = self.feedForward(X[h])
                 dZ = Z[h]
-                error = dZ - Y[len(Y) - 1]
-                errors.append(error)
                 dW = self.backPropagation(Y, dY, dZ)
                 self.adaptation(dW)
-            meanError = np.mean(np.abs(errors))
+
+            y_pred = self.predict(X)
+
+            error = Z - y_pred
+
+            meanError = np.mean(np.abs(error), axis=0)
             learning.append(meanError)
             iters += 1
-            print(meanError)
-            print(iters)
+            print(
+                f"Mean error: {meanError} and : {np.mean(meanError)} on iter {iters}")
+
         return iters, meanError, learning
-
-
-
 
     def predict(self, X):
         ans = []
         for h in range(len(X)):
-            Y, dY = self.feedForward(X[h])                                      # get the output of each layer
-            ans.append(np.array(Y[-1][0]))                                         # save the output of the last layer
+            # get the output of each layer
+            Y, dY = self.feedForward(X[h])
+            # save the output of the last layer
+            ans.append(np.array(Y[-1][0]))
         return ans
-    
+
     # ========================= ACTIVATION FUNCTIONS AND THEIR DERIVATIVES =========================
     # EXAMPLE: x = sigmoid(something) => x[0] is the output and x[1] is the derivative
 
     # sigmoid activation function
-    def sigmoid(self, x):        
+    def sigmoid(self, x):
         t = 1 / (1 + np.exp(-x))
         dt = np.multiply(t, 1 - t)
         return t, np.array(dt)
@@ -104,11 +132,11 @@ class Model():
         t = np.where(x > 0, 1, 0)
         dt = np.ones(t.shape)
         return t, dt
-            
+
     # ========================= AUXILIARY =========================
 
     # adaptation function
-    def adaptation(self, dW): 
+    def adaptation(self, dW):
         for k in range(1, self.layers):
             self.W[k-1] = self.W[k-1] + dW[k-1]
 
@@ -117,16 +145,18 @@ class Model():
         return np.append(np.array(x), 1)
 
     # remove bias from the input
-    def subBias(self,Y):
-        return Y[:,:-1]
-
+    def subBias(self, Y):
+        return Y[:, :-1]
 
     def generateWeights(self, n, m):
         return np.matrix(np.random.uniform(-1, 1, (n, m)))
 
     # generate random weights
     def createRandomWeights(self):
-        w = []                                                       # array of weight matrices
+
+        # array of weight matrices
+        w = []
+
         for i in range(0, self.layers - 1):
             layerW = self.generateWeights(self.S[i] + 1, self.S[i + 1])
             w.append(layerW)
